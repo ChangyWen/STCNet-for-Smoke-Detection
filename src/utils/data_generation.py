22d4cb75be0b4e6caad01afcad0a4b0b27ef6b76
wendacheng <1125102114@qq.com>
2021-06-21 04:51:23 +0800

    data

---
+++ src/utils/data_generation.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+from util import load_json, split_list, random_scale, generate_random_crop_pos, random_crop_pad_to_shape
+from torch.utils.data import Dataset, DataLoader
+
+class BaseDataset(Dataset):
+    def __init__(self, split_name='all', img_height=224, img_width=224, mode='train', segs_per_clip=8):
+        assert split_name in ['all', 's0', 's1', 's2', 's3', 's4', 's5'], split_name
+        mapping = {'s0': 0, 's1': 1, 's2': 2, 's3': 3, 's4': 4, 's5': 5}
+        self.rgb_path = '../data/rgb/'
+        self.mode = mode
+        json_name = [
+            '../data/split/metadata_{}_split_0_by_camera.json',
+            '../data/split/metadata_{}_split_1_by_camera.json',
+            '../data/split/metadata_{}_split_2_by_camera.json',
+            '../data/split/metadata_{}_split_by_date.json',
+            '../data/split/metadata_{}_split_3_by_camera.json',
+            '../data/split/metadata_{}_split_4_by_camera.json',
+        ]
+        file_name = [f.format(self.mode) for f in json_name]
+        self.data_list = []
+        if split_name == 'all':
+            for f in file_name:
+                self.data_list.append(load_json(f))
+        else:
+            self.data_list.append(load_json(file_name[mapping[split_name]]))
+        self.img_height = img_height
+        self.img_width = img_width
+
+        '''for convenience'''
+        self.segs_per_clip = segs_per_clip
+        num_frames = 36
+        self.segs = split_list(num_frames, self.segs_per_clip + 1)
+        self.alpha_array = [0.5, 0.75, 1, 1.25, 1.5]
+        self.beta_array = [-70, -35, 0, 35, 70]
+        self.scale_array = [0.5, 0.75, 1, 1.5, 1.75, 2.0]
+        self.flip_array = [-1, 0, 1]
+        self.rotate_array = [0, 2]
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        info = self.data_list[index]
+        video_frames = load_json(self.rgb_path + info['file_name'] + '.npy')
+        indices = [np.random.choice(seg) for seg in self.segs]
+        temp = self.preprocess(video_frames[indices, :, :, :])
+        frames = temp[:-1]
+        res_frames = temp[1:] - frames
+        label = info['label']
+        return (frames, res_frames, label)
+
+    def preprocess(self, frames):
+        processed_frames = []
+        alpha = np.random.choice(self.alpha_array) if self.alpha_array else 1
+        beta = np.random.choice(self.beta_array) if self.beta_array else 0
+        scale = np.random.choice(self.scale_array) if self.scale_array else 1
+        flip = np.random.choice(self.flip_array)
+        rotate = np.random.choice(self.rotate_array)
+        use_flip = np.random.random() <= 0.5
+        for i in range(frames.shape[0]):
+            frame = frames[i]
+            frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
+            frame = cv2.flip(frame, flip) if use_flip else cv2.rotate(frame, rotate)
+            frame = random_scale(frame, scale)
+            crop_size = (self.img_height, self.img_width)
+            crop_pos = generate_random_crop_pos(frame.shape[:2], crop_size)
+            frame = random_crop_pad_to_shape(frame, crop_pos, crop_size, 0)
+            processed_frames.append(frame)
+        return np.stack(processed_frames, axis=0)
+
+def get_DataLoader(mode='train', batch_size=4, drop_last=True, is_shuffle=True, pin_memory=True):
+    dataset = BaseDataset(mode=mode)
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        drop_last=drop_last,
+        shuffle=is_shuffle,
+        pin_memory=pin_memory,
+    )
+    return data_loader
--- src/utils/util.py
+++ src/utils/util.py
@@ -27,6 +27,8 @@ import uuid
 import copy
 import time
 from collections import OrderedDict
+import cv2
+import collections
 
 
 # Given an array of zeros and ones, output a list of events [[start_1, end_1], [start_2, end_2], ...]
@@ -422,3 +424,68 @@ def load_model(model, model_file, is_restore=False):
             t_ioend - t_start, t_end - t_ioend))
 
     return model
+
+
+def split_list(target_list, n):
+    if isinstance(target_list, int):
+        target_list = [i for i in range(target_list)]
+    k, m = divmod(len(target_list), n)
+    return (target_list[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+def random_scale(img, scale):
+    sh = int(img.shape[0] * scale)
+    sw = int(img.shape[1] * scale)
+    img = cv2.resize(img, (sw, sh), interpolation=cv2.INTER_LINEAR)
+    return img
+
+def generate_random_crop_pos(ori_size, crop_size):
+    ori_size = get_2dshape(ori_size)
+    h, w = ori_size
+    crop_size = get_2dshape(crop_size)
+    crop_h, crop_w = crop_size
+    pos_h, pos_w = 0, 0
+    if h > crop_h:
+        pos_h = np.random.randint(0, h - crop_h + 1)
+    if w > crop_w:
+        pos_w = np.random.randint(0, w - crop_w + 1)
+    return pos_h, pos_w
+
+def get_2dshape(shape, *, zero=True):
+    if not isinstance(shape, collections.Iterable):
+        shape = int(shape)
+        shape = (shape, shape)
+    else:
+        h, w = map(int, shape)
+        shape = (h, w)
+    if zero:
+        minv = 0
+    else:
+        minv = 1
+    assert min(shape) >= minv, 'invalid shape: {}'.format(shape)
+    return shape
+
+def pad_image_to_shape(img, shape, border_mode, value):
+    margin = np.zeros(4, np.uint32)
+    shape = get_2dshape(shape)
+    pad_height = shape[0] - img.shape[0] if shape[0] - img.shape[0] > 0 else 0
+    pad_width = shape[1] - img.shape[1] if shape[1] - img.shape[1] > 0 else 0
+    margin[0] = pad_height // 2
+    margin[1] = pad_height // 2 + pad_height % 2
+    margin[2] = pad_width // 2
+    margin[3] = pad_width // 2 + pad_width % 2
+    img = cv2.copyMakeBorder(img, margin[0], margin[1], margin[2], margin[3],
+                             border_mode, value=value)
+    return img, margin
+
+def random_crop_pad_to_shape(img, crop_pos, crop_size, pad_label_value):
+    h, w = img.shape[:2]
+    start_crop_h, start_crop_w = crop_pos
+    assert ((start_crop_h < h) and (start_crop_h >= 0))
+    assert ((start_crop_w < w) and (start_crop_w >= 0))
+    crop_size = get_2dshape(crop_size)
+    crop_h, crop_w = crop_size
+    img_crop = img[start_crop_h:start_crop_h + crop_h,
+               start_crop_w:start_crop_w + crop_w, ...]
+    img_, margin = pad_image_to_shape(img_crop, crop_size, cv2.BORDER_REFLECT_101,
+                                      pad_label_value)
+    return img_, margin
