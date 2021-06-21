49de92343450bf7cc1af9a5d74a9a20a5c4e0f98
wendacheng <1125102114@qq.com>
2021-06-22 03:02:27 +0800

    update

--- src/data_preprocess.sh
+++ src/data_preprocess.sh
@@ -4,5 +4,5 @@ resolution=$1
 
 set -x
 python utils/split_metadata.py confirm ${resolution}
-python utils/download_videos.py
-python utils/process_videos.py
+python utils/download_videos.py ${resolution}
+python utils/process_videos.py ${resolution}
--- src/utils/download_videos.py
+++ src/utils/download_videos.py
@@ -22,6 +22,10 @@ json sample
 
 # Download all videos in the metadata json file
 def main(argv):
+    resolution = 180
+    if len(argv) > 1:
+        if argv[1] not in ['320', '180']: raise AttributeError('resolution')
+        resolution = int(argv[1])
     vm = load_json("../data/metadata.json")
     video_root_path = "../data/videos/"
     check_and_create_dir(video_root_path)
@@ -30,11 +34,19 @@ def main(argv):
         # Do not download videos with bad data
         if v["label_state"] == -2 or v["label_state_admin"] == -2:
             continue
-        file_path = video_root_path + v["file_name"] + ".mp4"
+        if resolution == 320:
+            file_name = v['file_name'].replace('-180-180-', '-320-320-')
+            url_root = v["url_root"].replace('/180/', '/320/')
+            url_part = v['url_part'].replace('-180-180-', '-320-320-')
+        else:
+            file_name = v["file_name"]
+            url_root = v["url_root"]
+            url_part = v['url_part']
+        file_path = video_root_path + file_name + ".mp4"
         if is_file_here(file_path): continue # skip if file exists
         print("Download video", v["id"])
         try:
-            urllib.request.urlretrieve(v["url_root"] + v["url_part"], file_path)
+            urllib.request.urlretrieve(url_root + url_part, file_path)
         except:
             print("\tError downloading video", v["id"])
             problem_video_ids.append(v["id"])
@@ -42,7 +54,7 @@ def main(argv):
     if len(problem_video_ids) > 0:
         print("The following videos were not downloaded due to errors:")
         for i in problem_video_ids:
-            print("\ti\n")
+            print("\t{}\n".format(i))
 
 
 if __name__ == "__main__":
