9b3eceb5a5f0091405b7bd12c51c91e06972a516
wendacheng <1125102114@qq.com>
2021-08-24 19:54:30 +0800

    check if up-to-date

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__=['utils', 'main', 'model']
-
-import os
-import sys
-import inspect
-cmd_subfolder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
